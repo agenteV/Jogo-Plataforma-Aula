77baa1e20d0be458c5d8bcfe207e74b6c5e33a25
Reimar <reimargamer@proton.me>
2024-09-02 11:28:36 -0300

    Marlon

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float horizontal;
+    private Rigidbody2D rb;
+    private bool isFacingRight = true;
+    public Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        this.rb.velocity = new Vector2(horizontal * 8f, rb.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+        Flip();
+
+        /*
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Espaço");
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("clicou");
+
+        }
+        */
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+
+    }
+
+}
+
